e6bc16333198044c461e7db32356add1dc103c85
lonerapier <lonerapier@proton.me>
2023-01-24 18:49:17 +0000

    add different dockerfile for same distro and different distro

--- reproducible-builds/run_aesm.sh
+++
@@ -1,3 +0,0 @@
-#! bin/sh
-
-nohup /opt/intel/sgx-aesm-service/aesm/aesm-service --no-daemon &
--- reproducible-builds/run_in_docker.sh
+++ reproducible-builds/run_in_docker.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 set -e
-sudo docker build --build-arg https_proxy=$https_proxy \
-             --build-arg http_proxy=$http_proxy -t sgx_gramine -f ./Dockerfile .
+sudo docker build -t diffoscope -f ./Dockerfile-same-distro .
+sudo docker build -t diffoscope -f ./Dockerfile-diff-distro .
 
-sudo docker volume create --driver local --opt type=tmpfs --opt device=tmpfs --opt o=rw aesmd-socket
-
-sudo docker run --env http_proxy --env https_proxy --device=/dev/sgx_enclave --device=/dev/sgx_provision -v /dev/log:/dev/log -v aesmd-socket:/var/run/aesmd -it sgx_gramine
+sudo docker run --rm -t -w /tmp diffoscope ./ubuntu20/ ./ubuntu22/
