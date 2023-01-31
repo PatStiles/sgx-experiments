6891325f1af256c7c850804b06ff527f2e1e8abd
lonerapier <lonerapier@proton.me>
2023-01-31 18:19:32 +0000

    add `exclude-directory-metadata` flag

--- reproducible-builds/run_in_docker.sh
+++ reproducible-builds/run_in_docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 sudo docker build -t diffoscope -f ./Dockerfile-same-distro .
-sudo docker run --rm --read-only --tmpfs /tmp -t diffoscope /gramine1/ /gramine2/ > result-same-distro.diff
+sudo docker run --read-only --tmpfs /tmp -t diffoscope --exclude-directory-metadata=yes /gramine1/ /gramine2/ > result-same-distro.diff
 
 # sudo docker build -t diffoscope -f ./Dockerfile-diff-distro .
-# sudo docker run --rm --read-only --tmpfs /tmp -t diffoscope /ubuntu20/ /ubuntu22/ > result-diff-distro.diff
+# sudo docker run --rm --read-only --tmpfs /tmp -t diffoscope --exclude-directory-metadata=yes /ubuntu20/ /ubuntu22/ > result-diff-distro.diff
